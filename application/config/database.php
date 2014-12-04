aad6981da4a5c53c5ac2e96736a88f1fcab247f5
luokuncool <1131915347@qq.com>
2014-12-04 23:21:33 +0800

    commit

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '192.168.1.106';
+$db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'ci_easyui_sys';
